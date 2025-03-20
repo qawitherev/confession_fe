020f37f8c3a3f533d89cb14e8c403123c674dcf5
qawitherev <qawitherev@gmail.com>
2025-03-20 12:25:27 +0800

    hhh

--- src/pages/MyConfessions.js
+++ src/pages/MyConfessions.js
@@ -9,7 +9,7 @@ import ConfessionService from "../services/confessionService";
 import { useEffect, useState } from "react";
 
 const MyConfessions = () => {
-    const [activeTab, setActiveTab] = useState("account");
+    const [activeTab, setActiveTab] = useState("Pending");
     const [loading, setLoading] = useState(false);
     const handleChange = (event, _) => {
         setActiveTab(event);
@@ -19,6 +19,15 @@ const MyConfessions = () => {
         setLoading(true);
         try {
             const res = await ConfessionService.getConfessionsForUser(activeTab);
+            const data = res[0].map(d => ({
+                title: d.username,
+                body: d.title,
+                body: d.body,
+                tags: d.tags.split(',').map(t => t.trim()),
+                createdAt: new Date(d.createdAt).toLocaleDateString('en-US'),
+                rejectedBy: d.rejectedBy,
+                rejectedAt: new Date(d.rejectedAt).toLocaleDateString('en-US')
+            })); 
         } catch {
 
         } finally {
@@ -31,22 +40,25 @@ const MyConfessions = () => {
     }, [activeTab]);
 
   return (
-    <Tabs
-      defaultValue="account"
-      className="w-full max-w mx-auto mt-8 bg-red-300 rounded-lg shadow-lg p-4"
-      onValueChange={handleChange}
-    >
-      <TabsList>
-        <TabsTrigger value="Pending">Pending Confessions</TabsTrigger>
-        <TabsTrigger value="Published">Published Confessions</TabsTrigger>
-        <TabsTrigger value="Rejected">Rejected Confessions</TabsTrigger>
-        <TabsTrigger value="Deleted">Deleted Confessions</TabsTrigger>
-      </TabsList>
-      <TabsContent value="Pending">Make changes</TabsContent>
-      <TabsContent value="Published">Change your password here.</TabsContent>
-      <TabsContent value="Rejected">Change your email here.</TabsContent>
-      <TabsContent value="Deleted">Change your email here.</TabsContent>
-    </Tabs>
+    <div className="flex flex-col gap-4 w-full overflow-y-auto p-4">
+        <h1 className="text-xl font-semibold">My Confessions</h1>
+        <Tabs
+          defaultValue="Pending"
+          className="w-full max-w mx-auto mt-8 bg-red-300 rounded-lg shadow-lg p-4"
+          onValueChange={handleChange}
+        >
+          <TabsList>
+            <TabsTrigger value="Pending">Pending Confessions</TabsTrigger>
+            <TabsTrigger value="Published">Published Confessions</TabsTrigger>
+            <TabsTrigger value="Rejected">Rejected Confessions</TabsTrigger>
+            <TabsTrigger value="Deleted">Deleted Confessions</TabsTrigger>
+          </TabsList>
+          <TabsContent value="Pending">Make changes</TabsContent>
+          <TabsContent value="Published">Change your password here.</TabsContent>
+          <TabsContent value="Rejected">Change your email here.</TabsContent>
+          <TabsContent value="Deleted">Change your email here.</TabsContent>
+        </Tabs>
+    </div>
   );
 };
 
